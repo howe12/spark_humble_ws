95171887992943ca033fa28a68442cd56f22b52f
haijie-huo <howe12@126.com>
2026-06-16 15:39:42 +0800

    fix: add missing DeclareLaunchArgument import in swiftpro launch

--- src/spark_driver/arm/uArm/swiftpro/launch/pro_control_nomoveit.launch.py
+++ src/spark_driver/arm/uArm/swiftpro/launch/pro_control_nomoveit.launch.py
@@ -19,6 +19,7 @@ import os
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 
+from launch.actions import DeclareLaunchArgument
 from launch.substitutions import LaunchConfiguration
 import launch_ros.actions
 from launch_ros.actions import Node
